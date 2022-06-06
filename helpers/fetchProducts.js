00d36bc2ef6a4250790c7aaf873e9effb84f2375
thais <thaisviana081@outlook.com>
2022-06-06 09:41:46 -0300

    requisito 1,2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  try {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+    const response = await fetch(endpoint).then((resposta) => resposta.json());
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
