dfe29c59b956f5ce0948e197108c5a69428c5752
thais <thaisviana081@outlook.com>
2022-06-07 10:18:32 -0300

    ajuste

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,14 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
+    const data = await response.json();
+    const results = await data;
+    return results;
+  } catch (error) {
+    return error;
+  }
 };
-
+fetchItem();
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
--- script.js
+++ script.js
@@ -1,35 +1,34 @@
+// criando tag img com class e a imagem
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 };
-
+// cria tags com class e text
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 };
-
+// cria os itens com botao para adicionar no carrinho id,titulo e imagem
 const createProductItemElement = ({ sku, name, image }) => {
-  const section = document.createElement('section');
+   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 };
-
+// selecionando e exibindo o item
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
 };
-
+// criando elementos li com id titulo e preço e chando funçao q add esses elementos dentro do carrinho
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -37,5 +36,7 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.addEventListener('click', cartItemClickListener);
   return li;
 };
+
+
 
 window.onload = () => { };
